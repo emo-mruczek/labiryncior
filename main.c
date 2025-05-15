5e8f88104f44f9d9d4ed9c110445fca44addcbcc
emo-mruczek <krokcia1@gmail.com>
2025-05-15 12:43:35 +0200

    add: creating png structs

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <png.h>
 
 #define HEADER_SIZE 8
+#define PNG_LIBPNG_VER_STRING "1.6.46"
 
 int main(int argc, char** argv) {
 
@@ -20,18 +21,35 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    uint8_t header[HEADER_SIZE] = {0, 0, 0, 0, 0, 0, 0, 0}; // TODO:replace with calloc lol cuz im insane
+    uint8_t header[HEADER_SIZE] = {0, 0, 0, 0, 0, 0, 0, 0};
 
     if (fread(header, sizeof(header[0]), HEADER_SIZE, fp) != HEADER_SIZE) {
         fprintf(stdout, "error reading header bytes\n");
         exit(EXIT_FAILURE);
     } 
 
-    /* check whether file is a png -> png_sig_comp() */
+    /* check whether file is a png */
     
     bool is_png = (png_sig_cmp(header, 0, HEADER_SIZE) == 0); 
     if (!is_png) {
         fprintf(stdout, "not a png!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* initializing the necessary structures */
+
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+    if (!png_ptr) {
+        fprintf(stdout, "failed to create a png struct!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    png_infop png_info_ptr = png_create_info_struct(png_ptr);
+
+    if (!png_info_ptr) {
+        fprintf(stdout, "couldn't create a png info struct!\n");
+        png_destroy_read_struct(&png_ptr, NULL, NULL);
         exit(EXIT_FAILURE);
     }
 
